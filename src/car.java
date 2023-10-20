16c5656aac39b786373e92d03e2bc4289c3fe709
CANO <umutcanbozyigit34012@gmail.com>
2023-10-20 14:32:19 +0300

    class yapısı

---
+++ src/car.java
@@ -0,0 +1,19 @@
+ class car {
+    String type;
+    String model;
+    String color;
+    int speed;
+    int speedLimit;
+    void increaseSpeed(int increment){
+        speed +=increment;
+    }
+    void  decreaseSpeed(int decrease)
+    {
+        if(speed>0){
+            speed-=decrease;
+        }
+    }
+    void printSpeed(){
+        System.out.println(("hızınız :"+speed));
+    }
+ }
